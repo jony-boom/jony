888c4458ab9ff5e01a8e4974d22408f6a9587e2a
jony-boom <jonyuldoshev06@gmail.com>
2024-09-18 12:52:02 +0500

    Add files via upload

---
+++ # №2.py
@@ -0,0 +1,12 @@
+# №2
+
+x = float(input())
+y = float(input())
+
+if x > y:
+    z = x - y
+else:
+    z = y - x + 1
+
+print(z)
+    
---
+++ 1.py
@@ -0,0 +1,13 @@
+import math
+
+x = float(input("Введите число:"))
+a = x * x
+b = a * x
+c = a * 3
+d = b * 4
+e = x + 2
+j = 1 + e
+h = j + c
+g = h + d
+
+print(g)
